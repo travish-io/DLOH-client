68471c78ab8992876ef011453ed169b46304f49f
Travis Hall <t.hall89d@gmail.com>
2021-12-09 21:36:14 -0500

    basic setup

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { NavBar } from "./nav/NavBar";
+
+export const ApplicationViews = () => {
+  return (
+    <>
+      {/* <Route path="/">
+        <NavBar />
+      </Route> */}
+    </>
+  );
+};
---
+++ src/components/auth/Register.js
@@ -0,0 +1,103 @@
+import React, { useRef } from "react";
+import { Link, useHistory } from "react-router-dom";
+import "./Auth.css";
+
+export const Register = () => {
+  const username = useRef();
+  const password = useRef();
+  const verifyPassword = useRef();
+  const passwordDialog = useRef();
+  const history = useHistory();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (password.current.value === verifyPassword.current.value) {
+      const newUser = {
+        username: username.current.value,
+        password: password.current.value,
+      };
+
+      return fetch("http://127.0.0.1:8000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(newUser),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if ("token" in res) {
+            localStorage.setItem("dloh_token", res.token);
+            history.push("/");
+          }
+        });
+    } else {
+      passwordDialog.current.showModal();
+    }
+  };
+
+  return (
+    <main style={{ textAlign: "center" }}>
+      <dialog className="dialog dialog--password" ref={passwordDialog}>
+        <div>Passwords do not match</div>
+        <button
+          className="button--close"
+          onClick={(e) => passwordDialog.current.close()}
+        >
+          Close
+        </button>
+      </dialog>
+
+      <form className="form--login" onSubmit={handleRegister}>
+        <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
+        <fieldset>
+          <label htmlFor="inputUsername">Username</label>
+          <input
+            ref={username}
+            type="text"
+            name="username"
+            className="form-control"
+            placeholder="Username"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="inputPassword"> Password </label>
+          <input
+            ref={password}
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder="Password"
+            required
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="verifyPassword"> Verify Password </label>
+          <input
+            ref={verifyPassword}
+            type="password"
+            name="verifyPassword"
+            className="form-control"
+            placeholder="Verify password"
+            required
+          />
+        </fieldset>
+        <fieldset
+          style={{
+            textAlign: "center",
+          }}
+        >
+          <button className="btn btn-1 btn-sep icon-send" type="submit">
+            Register
+          </button>
+        </fieldset>
+      </form>
+      <section className="link--register">
+        Already registered? <Link to="/login">Login</Link>
+      </section>
+    </main>
+  );
+};
