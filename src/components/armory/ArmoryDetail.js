2647e854c9d1376a181d55022080c813b62e373c
Travis Hall <t.hall89d@gmail.com>
2021-12-20 15:10:37 -0500

    .

--- src/components/armory/ArmoryDetail.js
+++ src/components/armory/ArmoryDetail.js
@@ -22,14 +22,7 @@ export const ArmoryDetail = (itemHash, toggleDetail, setToggleDetail) => {
       {console.log(itemHash)}
       <div>
         <h3>{item?.Response?.displayProperties?.name}</h3>
-        <button
-          onClick={() => {
-            setToggleDetail(false);
-            return toggleDetail;
-          }}
-        >
-          Close
-        </button>
+        <button onClick={setToggleDetail(!toggleDetail)}>Close</button>
       </div>
       <img
         src={`https://www.bungie.net${item?.Response?.displayProperties?.icon}`}
